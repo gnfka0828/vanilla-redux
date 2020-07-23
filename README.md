76e46092aaaf56af9df41887b8b02f7daaf27a90
hooram.seo <hooram.seo@lge.com>
2020-07-24 00:04:51 +0900

    feat : To Do List Initialize, etc.
    1) To do List Initialize
    2) Add Redux Reducer, Store, Subscribe

--- README.md
+++ README.md
@@ -4,4 +4,9 @@
 Learning Vanilla-Redux and React-Redux
 
 https://geonlee.tistory.com/32 : npx 
+
 https://velopert.com/241 : global module 설치 시 link해 줘야 함
+
+https://paulkogel.gitbooks.io/redux-docs/content/docs/introduction/ThreePrinciples.html : Three Principles of Redux
+
+https://medium.com/@ca3rot/redux-%EC%9E%85%EB%AC%B8-10%EB%B6%84%EC%9C%BC%EB%A1%9C-%EC%9D%B4%ED%95%B4%ED%95%98%EB%8A%94-redux-%EA%B8%B0%EC%B4%88-3-a18222084f3a : Reducer 공식문서
--- src/index.js
+++ src/index.js
@@ -1,54 +1,45 @@
 import { createStore } from "redux";
 // Store 내에 있는 함수
 // dispatch, subscribe, getState, replaceReducer
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-number.innerText = 0;
-
-const ADD = "ADD";
-const MINUS = "MINUS";
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
 // Data를 바꾸고 Modifiy하는 것을 책임짐.(Modify의 의미를 찾고 수정함)  
 // reducer나 modifier는 처음으로 Data를 변경해 줌.
 // return 값 : Application의 Data
 // redux : 유일하게 reducer로만 Data 값을 변경할 수 있다.
 // action으로 어떨 때 해당 data 값을 변경하는지 지시할 수 있다. (dispatch 이용)
-const countModifier = (count = 0, action) => {
+const reducer = (state = [], action) => {
     // ... modify state
 
     // Refactor 
     // 1. Reducer에 if문 대신 switch문 사용
     // 2. action type을 미리 선언하여 사용(틀리게 입력 시 오류가 나서 알 수 있게 됨, 안전)
     switch (action.type) {
-        case ADD:
-            return count + 1;
-        case MINUS:
-            return count - 1;
+        case ADD_TODO:
+            return [...state, { text: action.text, id: Date.now() }];
+        case DELETE_TODO:
+             return [];
         default:
-            return count;
+             return state;
     }
 };
 
-// countModifier를 initial state로 불러옴. => state 기본값 지정 가능
-const countStore = createStore(countModifier);
+// reducer를 initial state로 불러옴. => state 기본값 지정 가능
+const store = createStore(reducer);
 
-const onChange = () => {
-    number.innerText = countStore.getState();
-}
+store.subscribe(() => console.log(store.getState()));
 
-countStore.subscribe(onChange);
-
-const handleAdd = () =>{
-    countStore.dispatch({ type: ADD });
-}
-
-const handleMinus = () =>{
-    countStore.dispatch({ type: MINUS });
-}
+const onSubmit = e => {
+    e.preventDefault();
+    const toDo = input.value;
+    input.value = "";
+    store.dispatch({ type: ADD_TODO, text: toDo });
+};
 
 // 두번째 인자로 function을 줘야 한다.
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
