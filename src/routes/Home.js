2a65981a26e130e3addb4122a0b8057ba4ba6a79
yeon.hoo <gnfka0828@gmail.com>
2020-07-28 09:55:49 +0900

    feat : mapDispatchToProps
    feat : mapDispatchToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-function Home(props) {
+function Home({ toDos, addToDo }) {
     const [text, setText] = useState("");
-    console.log(props);
 
     function onChange(e) {
         setText(e.target.value);
@@ -12,7 +12,7 @@ function Home(props) {
     function onSubmit(e) {
         // onSubmit 기본 기능 : Refresh를 막기 위한 코드
         e.preventDefault();
-        console.log(text);
+        addToDo(text);
         setText("");
     }
 
@@ -24,7 +24,7 @@ function Home(props) {
                 <input type="text" value={text} onChange={onChange}/>
                 <button>Add</button>
             </form>
-            {/* <ul>{JSON.stringify(toDos)}</ul> */}
+            <ul>{JSON.stringify(toDos)}</ul>
         </>
     );
 }
@@ -33,7 +33,15 @@ function mapStateToProps(state) {
     return { toDos: state };
 }
 
+// mapDispatchToProps : 함수를 Props로 Component에 전달.
+function mapDispatchToProps(dispatch) {
+    return {
+      // addToDo 함수를 만들어 전달 : text라는 변수를 받아 actionCreators.addToDo 함수에 넣어 action 객체를 만들어 dispatch.
+      addToDo: text => dispatch(actionCreators.addToDo(text))
+    };
+  }
+
 // mapStateToProps 함수를 이용하여 Home Component에 Prop 전달.
 // 전달되는 Props : react-router에서 온 Props(Router, Route)
 //                + mapStateToProps에서 return되는 값.
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
